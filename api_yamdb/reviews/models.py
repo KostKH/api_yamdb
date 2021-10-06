d4054cb2ee3302407e339808606c4794e0ce199f
Alex-K-YP <oleksandrkolbasuk@gmail.com>
2021-10-06 23:01:10 +0300

    add reviews.models(Comments, Review)

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
@@ -21,3 +22,45 @@ class User(AbstractUser):
 
     class Meta:
         ordering = ['-id']
+
+
+class Review(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='author',
+        null=False)
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации',
+        auto_now_add=True)
+    score = models.PositiveIntegerField(
+        default=10,
+        validators=[MinValueValidator(1), MaxValueValidator(10)],
+        verbose_name='оценка')
+    title = models.ForeignKey(
+        "Titles",
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL)
+    text = models.TextField(
+        verbose_name='текст оценки',
+        help_text='оцените произвидение')
+
+
+class Comments(models.Model):
+    author = models.ForeignKey(
+        "User",
+        on_delete=models.CASCADE,
+        verbose_name='author',
+        null=False)
+    pub_date = models.DateTimeField(
+        verbose_name='Дата публикации',
+        auto_now_add=True)
+    review = models.ForeignKey(
+        "Review",
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE)
+    text = models.TextField(
+        verbose_name='текст коментария',
+        help_text='добавьте коментарий')
