17cf35be318f150075867928d61fbc0647b5b206
MaksLatortcev <maksim_anatolievish@mail.ru>
2021-10-06 22:13:04 +0300

    add models 06.10.2021

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -21,3 +21,33 @@ class User(AbstractUser):
 
     class Meta:
         ordering = ['-id']
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Categories(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Titles(models.Model):
+    name = models.CharField(max_length=200)
+    year = models.DateField()
+    genres = models.ManyToManyField("Genre", related_name="titles")
+    categories = models.ManyToManyField("Categories",
+                                        related_name="titles")
+
+    class Meta:
+        ordering = ["year"]
+
+    def __str__(self):
+        return self.name
