0017f047eccf681acd477de9fa720f9deee06cd4
Alex-K-YP <oleksandrkolbasuk@gmail.com>
2021-10-11 00:22:24 +0300

    comments & review tests passed

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -9,8 +9,12 @@ router_v1 = DefaultRouter()
 router_v1.register('genres', views.GenreViewSet)
 router_v1.register('categories', views.CategoriesViewSet)
 router_v1.register('titles', views.TitlesViewSet)
+router_v1.register(r'titles/(?P<title_id>\d+)/reviews',
+                   views.ReviewViewSet, basename='reviews')
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    views.CommentViewSet, basename='comments')
 router_v1.register('users', views.UserViewSet)
-
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
